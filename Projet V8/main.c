7c0640a71cea3706888cf6c7e739353954d43350
Blazarothe <120842830+Blazarothe@users.noreply.github.com>
2023-01-08 19:47:17 +0100

    Add files via upload

---
+++ Projet V8/main.c
@@ -0,0 +1,20 @@
+#include "head.h"
+
+
+
+int main() {
+    int matrix[49][3][3];
+    int choix;
+    int nbr_joueurs;
+
+
+    choix = menu();
+    conditions(choix,&nbr_joueurs,matrix);
+    // tresors_joueurs(nbr_joueurs,J1);
+
+    // for(int i=0;i<24/nbr_joueurs;i++){
+    //printf("\n%d sz",*J1[i]);}
+
+
+
+}
