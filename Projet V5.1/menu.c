40f87db08213ccd44997a3f5d42ad01c86ce5136
Altawn <121050054+Altawn@users.noreply.github.com>
2023-01-08 16:07:31 +0100

    Update menu.c

--- Projet V5.1/menu.c
+++ Projet V5.1/menu.c
@@ -48,7 +48,7 @@ int menu()
         return rep_menu;
 }
 
-void conditions(int choix,int *nmbre_joueurs,int nbr_joueurs,int matrix[49][3][3])
+void conditions(int choix, int* nmbre_joueurs, int matrix[49][3][3])
 {
     int sauvegarde = 0;
     char Oui_ou_Non[4];
@@ -76,12 +76,12 @@ void conditions(int choix,int *nmbre_joueurs,int nbr_joueurs,int matrix[49][3][3
             else
             {
                 choix = menu();
-                conditions(choix,nmbre_joueurs,nbr_joueurs,matrix);
+                conditions(choix,nmbre_joueurs,matrix);
             }
         }
         printf("Demarrage d'une nouvelle partie...\n");
         partie_debut = 1; //Ca va servir pour blinder la sauvegarde (ligne 70)
-        nouvelle_partie( nmbre_joueurs,matrix,nbr_joueurs); //Sous-programme Nouvelle partie (Alternative #2)
+        nouvelle_partie(nmbre_joueurs,matrix); //Sous-programme Nouvelle partie (Alternative #2)
     }
 
     if(choix == 2)
@@ -100,7 +100,7 @@ void conditions(int choix,int *nmbre_joueurs,int nbr_joueurs,int matrix[49][3][3
             system("cls"); //clear screen
             Sleep(500);
             choix = menu();
-            conditions(choix,nmbre_joueurs,nbr_joueurs,matrix);
+            conditions(choix,nmbre_joueurs,matrix);
         }
 
         else
@@ -129,7 +129,7 @@ void conditions(int choix,int *nmbre_joueurs,int nbr_joueurs,int matrix[49][3][3
             system("cls"); //clear ecran
             Sleep(500);
             choix = menu();
-            conditions(choix,nmbre_joueurs,nbr_joueurs,matrix);
+            conditions(choix,nmbre_joueurs,matrix);
         }
         else
         {
@@ -154,9 +154,9 @@ void color(int couleur_texte,int fond)
     SetConsoleTextAttribute(H,fond*16+couleur_texte);
 }
 
-void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3],int nbr_joueurs) {
+void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3]) {
 
-    int i;
+    int i, action, deplacement;
     int num_pion[4];
     char nom_personnage[nombre_joueurs_max][le_prenom_devient_long];
 
@@ -165,7 +165,7 @@ void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3],int nbr_joueurs) {
     Sleep(500);
     printf("\n                            [L]  [e]    [L]  [a]  [b]  [y]  [r]  [i]  [n]  [t]  [h]  [e]\n\n\n");
     printf("                          Ce jeu ne peut se jouer que de 2 a 4 joueurs... Combien etes vous : \n\n");
-    printf("                                                    Nous sommes ");
+    printf("                                                       Nous sommes ");
     scanf("%i", nmbre_joueurs);
     Sleep(150);
     system("cls");
@@ -197,7 +197,7 @@ void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3],int nbr_joueurs) {
     printf("                                                     3. L'elfe\n\n");
 
     color(14, 0); //pion jaune-fluo
-    printf("                                                    4. Le nain\n\n\n");
+    printf("                                                     4. Le nain\n\n\n");
 
     for (i = 0; i < *nmbre_joueurs; i++)
     {
@@ -208,10 +208,13 @@ void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3],int nbr_joueurs) {
         if (num_pion[i] != 1 && num_pion[i] != 2 && num_pion[i] != 3 && num_pion[i] != 4) //Blindage choisissage de pion
         {
             do {
+                Sleep(150);
+                system("cls");
+                Sleep(150);
                 fflush(stdin);
                 printf("\n                            [L]  [e]    [L]  [a]  [b]  [y]  [r]  [i]  [n]  [t]  [h]  [e]\n\n\n");
-                printf("                                   Ce personnage n'existe pas (pions : [1, 2, 3, 4]) :\n\n");
-                printf("                                                    Je choisis le pion  ");
+                printf("                                  Ce personnage n'existe pas (pions : [1, 2, 3, 4]) :\n\n");
+                printf("                                                Je choisis le pion  ");
                 scanf("%i", &num_pion[i]);
             } while (num_pion[i] != 1 && num_pion[i] != 2 && num_pion[i] != 3 && num_pion[i] != 4);
         }
@@ -226,7 +229,7 @@ void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3],int nbr_joueurs) {
                 scanf("%i", &num_pion[i]);
             } while (num_pion[i] == num_pion[i-1] || num_pion[i] == num_pion[i-2] ||num_pion[i] == num_pion[i-3]);
         }
-        printf("Comment se nommera votre personnage ?\n"); //Definition du nom du personnage
+        printf("*  Comment se nommera votre personnage ?\n"); //Definition du nom du personnage
         scanf("%s", &nom_personnage[i][le_prenom_devient_long]);
     }
     resume_pions(*nmbre_joueurs, num_pion, nom_personnage); //Sous-programme faisant apparaitre un resume des personnages
@@ -234,18 +237,107 @@ void nouvelle_partie(int *nmbre_joueurs,int matrix[49][3][3],int nbr_joueurs) {
     printf("Nous pouvons donc commencer a explorer ce labyrinthe remplie de mysteres, de creatures mais surtout de tresors...");
 
     int player = 3;
-    zebi(matrix,nbr_joueurs);
-    while(nbr_joueurs !=0)
+    zebi(matrix,*nmbre_joueurs);
+    while(nmbre_joueurs !=0)
     {
         depla(matrix,player);
+        for (i = 0; i < *nmbre_joueurs; i++)
+        {
+            printf("                                       * joueur %i, c'est à ton tour : \n", i+1);
+            printf("Commence par deplacer une ligne du labyrinthe...\n");
+            system("pause"); //Deplacer + Clear + Afficher le nouveau labyritnhe
+            printf("Ensuite, voici les differentes options que tu peux effectuer :\n\n");
+            printf("                                     -------------------------------------\n");
+            printf("                                    |            1.Te Deplacer            |\n");
+            printf("                                     -------------------------------------\n\n");
+            printf("                                     -------------------------------------\n");
+            printf("                                    |   2.Retourner au menu (Fin partie)  |\n");
+            printf("                                     -------------------------------------\n\n");
+            printf("                                     -------------------------------------\n");
+            printf("                                    |             3. Fin du Tour          |\n");
+            printf("                                     -------------------------------------\n\n");
+            printf("Quel sera votre choix : ");
+            scanf("%i", &action);
+            if (action == 1)
+            {
+                do
+                {
+                    printf("* Appuyez sur Z pour aller tout droit\n"
+                           "* Appuyez sur Q pour aller a gauche\n"
+                           "* Appuyez sur D pour aller a droite\n"
+                           "* Appuyez sur S pour reculer\n");
+                    depla(matrix,*nmbre_joueurs);
+                    printf("Voulez-vous continuer a vous deplacer ? (1 = oui, 0 = non) : ");
+                    scanf("%i", &deplacement);
+                }while(deplacement == 1);
+            }
+        }
     }
 
 }
 
 void regles_et_credits()
 {
+    int choix;
+    int *nmbre_joueurs, nbr_joueurs, matrix[49][3][3];
     system("cls");
-    printf("Voila, ce sera ici.");
+    Sleep(150);
+    printf("\n                            [L]  [e]    [L]  [a]  [b]  [y]  [r]  [i]  [n]  [t]  [h]  [e]\n\n\n");
+    color(12, 0);
+    printf("*    But du jeu.\n\n");
+    color(15, 0);
+    printf("    Vous penetrez dans un labyrinthe enchante a la recherche de fabuleux tresors et de mysterieuses creatures.\n");
+    printf("Chacun d'entre vous essaye de creer des chemins pour atteindre le tresor qu'il convoite en faisant coulisser \nastucieusement les couloirs.");
+    printf("Le joueur qui aura trouve tous ses tresors et rejoint sa case depart le premier \nsera declare vainqueur.\n");
+    printf(" \n");
+    printf(" \n");
+    color(12, 0);
+    printf("*    Deroulement de la partie.\n\n");
+    color(15, 0);
+    printf("    Chaque joueur commence par regarder secretement la carte superieure de sa pile ; elle indique le premier tresor\nqu'il doit trouver. ");
+    printf("Le dernier joueur a avoir participe a une chasse aux tresors entame la partie. \n");
+    printf(" \n\n");
+    color(3, 0);
+    printf("*    Le tour d'un joueur se decompose en deux etapes :\n\n");
+    color(15, 0);
+    printf("    1) Modifier les couloirs.\n");
+    printf("    2) Deplacer son pion.\n");
+    printf(" \n");
+    printf("    Quand vient son tour, le joueur doit essayer d'atteindre la case sur laquelle figure le meme tresor que sur la cartesuperieure de sa pile.");
+    printf(" Pour cela, il modifie toujours d'abord le labyrinthe en inserant la plaque Couloir, \npuis deplace son pion. \n");
+    printf("\n\n");
+    color(3, 0);
+    printf("*    1. modification des couloirs \n");
+    color(15, 0);
+    printf("\n    Quand vient son tour, le joueur choisit une rangee et un sens pour la piece qu'il veut inserer puis indique \ndans la console la rangee et le sens qu'il souhaite pour sa piece");
+    printf("\n\n\n");
+    color(3, 0);
+    printf("*    2. Deplacement du pion\n\n");
+    color(15, 0);
+    printf("    Apres avoir modifie le labyrinthe, le joueur peut deplacer son pion jusqu'a n'importe quelle case en suivant \nun couloir ininterrompu.");
+    printf(" Il peut se deplacer aussi loin qu'il veut. Il est aussi permis de rester sur place. ");
+    printf("\nSi un joueur atteint le tresor recherche, il l'a trouve ! Le compteur lui comptera alors 1 point. ");
+    printf(" Le prochain tresor \nqu'il a a trouver lui est alors montre.\n");
+    printf(" \n");
+    color(4, 0);
+    printf("Conseil : Si un joueur ne peut pas atteindre directement son but, il peut se deplacer aussi loin qu'il veut de maniere a etre en meilleure position pour le tour suivant.\n");
+    printf(" \n");
+    printf("\n");
+    color(couleur_blanche, 0);
+    printf("Le gagnant est le premier joueur a trouver tout les tresors qui lui ont ete demander de trouver et a retourne à son point de depart\n\n");
+
+    color(12, 0);
+    printf("Fin de la partie...\n");
+    color(couleur_blanche, 0);
+    printf(" \n");
+
+    system("pause");
+    Sleep(150);
+    system("cls");
+    Sleep(150);
+    choix = menu();
+    conditions(choix, &nbr_joueurs,matrix);
+
 }
 
 void resume_pions(int joueurs, int numero[4], char nom[nombre_joueurs_max][le_prenom_devient_long])
